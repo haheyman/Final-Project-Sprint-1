9b25e7f44d494ef0c80ebc5c35d1dc79e899a8c7
haheyman <107065082+haheyman@users.noreply.github.com>
2024-03-08 10:32:25 -0600

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,7 @@
+class Creds:
+    hostname = 'cis3368-haley.crqa2q6uw5gy.us-east-1.rds.amazonaws.com'
+    uname = 'admin'
+    passwd = 'CIS3368SP24'
+    dbname = 'CIS3368Haley'
+    login_user = 'Admin'
+    login_pass = 'CIS3368S24'
